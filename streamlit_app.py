249c536fa0f0fc4341ea1b069e4b37d299028295
p-khamruen <165988610+p-khamruen@users.noreply.github.com>
2025-05-02 19:17:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+st.title("ทดสอบ Streamlit Cloud")
+st.write("แอปนี้รันบน Streamlit Community Cloud ผ่าน GitHub")
+
+name = st.text_input("ชื่อของคุณคืออะไร?")
+if name:
+    st.success(f"สวัสดี {name}!")
 
